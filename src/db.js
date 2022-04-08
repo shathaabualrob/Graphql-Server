381f83c2b81d1df84d03148873b596d7e04f7c7c
Shatha Abu AlRob <shatharub5@gmail.com>
2022-04-08 16:19:04 +0300

    latest 1

---
+++ src/db.js
@@ -0,0 +1,4 @@
+export let users = [
+    { id: 1, name: "shatha", email: 'shatha@gmail.com', age:22 },
+    { id: 1, name: "shatha", email: 'shatha@gmail.com', age:22 }
+];
--- src/resolvers.js
+++ src/resolvers.js
@@ -2,7 +2,14 @@ import {users} from "./db";
 
 const resolvers = {
     Query:{
-        hello: ()=> "Hello World!"
+        // hello: ()=> "Hello World!"
+        user: (parent, {id}, context, info) =>{
+            return users.find(user => user.id === id);
+        },
+        users: (parent, args, context, info) =>{
+            return users;
+        }
     }
 };
+
 export default resolvers;
